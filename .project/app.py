f56465b7ea5609259d15ff043426cf3e423713a3
eliannea <166654436+eliannea@users.noreply.github.com>
2024-04-20 21:00:04 -0600

    app.py

--- .project/app.py
+++ .project/app.py
@@ -5,7 +5,7 @@ car_data = pd.read_csv("vehicles_us.csv")
 hist_button = st.button ("Construir histograma")
 if hist_button:
     st.write("Creacion de un histograma para el conjunto de datos de anucios de venta de coches")
-    fig=px.hisotgrama(car_data, x="odometer")
+    fig=px.hisotgram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
 scatter_plot = st.button("Construir grafica de dispersion")
